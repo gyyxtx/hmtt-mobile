49ccbe6455fb6b8f615b58f18a748598617614b5
gyyxtx <1042766239@qq.com>
2019-07-29 09:49:38 +0800

    rem适配+动态rootValue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
   <div id="app">
-    Vue
     <van-button type="primary">主要按钮</van-button>
+    <div class="swy"></div>
     <router-view/>
   </div>
 </template>
 
 <style lang="less" scoped>
+.swy {
+  width: 100px;
+  height: 100px;
+  background-color: hotpink;
+}
 </style>
