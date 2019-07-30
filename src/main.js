07a32052359e874f777b68b10401e8735fc5d143
gyyxtx <1042766239@qq.com>
2019-07-30 09:39:53 +0800

    登录-表单验证-完成

--- src/main.js
+++ src/main.js
@@ -3,10 +3,22 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import Vant from 'vant'
+// 引入rem适配相关插件
 import 'vant/lib/index.css'
 import '../node_modules/amfe-flexible/index.js'
+// 引入表单验证插件vee-validate 及相关汉化插件
+import zhCN from 'vee-validate/dist/locale/zh_CN'
+import VeeValidate, { Validator } from 'vee-validate'
 Vue.use(Vant)
-
+Vue.use(VeeValidate)
+Validator.localize('zh_CN', zhCN)
+// vee-validate自定义校验规则
+// moblie为自定义校验名称
+Validator.extend('phone', {
+  // 校验提示信息field为默认的自定义校验名
+  getMessage: field => '请输入正确手机号',
+  validate: value => value.length === 11 && /^((13|14|15|17|18)[0-9]{1}\d{8})$/.test(value)
+})
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,11 +1,20 @@
 <template>
   <div class="login-continer">
+    <!-- 表单验证测试代码 -->
+    <!-- 自带校验 -->
+    <!-- <input v-validate="'required|email'" name="email" type="text">
+    <span>{{ errors.first('email') }}</span> -->
+    <!-- 自定义校验 -->
+    <!-- <input v-validate="'required|phone'" name="phone" type="text">
+    <span>{{ errors.first('phone') }}</span> -->
     <!-- 导航 -->
     <van-nav-bar title="登录" />
     <!-- 登录表单 -->
     <form action="/" method="POST">
         <van-cell-group>
             <van-field
+            v-validate="'required|phone'" name="phone"
+            :error-message="errors.first('phone')"
             v-model="user.mobile"
             required
             clearable
@@ -38,10 +47,19 @@ export default {
     async handleLogin () {
     // 判断请求是否成功，如果成功就执行try，如果失败，就执行catch
       try {
-        const res = await login(this.user)
-        console.log(res)
-        // 登录成功后将请求返回的token值，更新给token
-        this.$store.commit('setUser', res)
+        // 验证手机号
+        this.$validator.validate().then(async valid => {
+          // 如果验证失败，直接返回
+          if (!valid) {
+            console.log('error----')
+            return
+          }
+          // 验证成功，执行登录
+          const res = await login(this.user)
+          console.log(res)
+          // 登录成功后将请求返回的token值，更新给token
+          this.$store.commit('setUser', res)
+        })
       } catch (error) {
         console.log(error)
       }
